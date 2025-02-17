3630a6a1fa4c9eb736563a112f15551222a70234
Дмитрий Жданов <D.Zhdanov66@ya.ru>
2025-02-18 02:29:01 +0300

    Проверка сопряжения github с VS2022

--- functions.cpp
+++ functions.cpp
@@ -1,17 +1,16 @@
 #include "functions.h"
 
 std::vector<int> getCoefficients(std::string str) {
-    // ”дал€ем пробелы из строки
     str.erase(std::remove(str.begin(), str.end(), ' '), str.end());
     std::vector<int> coefficients;
 
     std::smatch match;
     // –егул€рное выражение:
-    // √руппа 1: ([+-]?\d*)      Ч коэффициент при x^2 (может быть пустой строкой, если записано просто "x^2")
+    // √руппа 1: ([+-]?\d*)       Ч коэффициент при x^2 (может быть пустой строкой, если записано просто "x^2")
     // «атем: \*?x\^2             Ч сама переменна€ x^2 (опциональна€ звездочка)
-    // (?:([+-](\d*))\*?x)?      Ч опциональна€ группа дл€ x: если присутствует, то захватываем коэффициент (группа 2)
-    // (?:([+-]\d+))?           Ч опциональна€ группа дл€ свободного члена на левой части (группа 3)
-    // =([+-]?\d+)$            Ч права€ часть уравнени€ (группа 4)
+    // (?:([+-](\d*))\*?x)?       Ч опциональна€ группа дл€ x: если присутствует, то захватываем коэффициент (группа 2)
+    // (?:([+-]\d+))?             Ч опциональна€ группа дл€ свободного члена на левой части (группа 3)
+    // =([+-]?\d+)$               Ч права€ часть уравнени€ (группа 4)
     std::regex pattern("^([+-]?\\d*)\\*?x\\^2(?:([+-](\\d*))\\*?x)?(?:([+-]\\d+))?=([+-]?\\d+)$");
 
     if (std::regex_match(str, match, pattern)) {
@@ -29,7 +28,6 @@ std::vector<int> getCoefficients(std::string str) {
         int b = 0;
         if (match[2].matched) {
             std::string b_str = match[2].str();
-            // ≈сли после знака нет числа, то интерпретируем как 1 или -1
             if (b_str == "+" || b_str == "")
                 b = 1;
             else if (b_str == "-")
@@ -53,9 +51,7 @@ std::vector<int> getCoefficients(std::string str) {
         // √руппа 4: значение правой части уравнени€
         int right_side = std::stoi(match[5].str());
 
-        // ѕриводим уравнение к виду ax^2 + bx + c = 0, вычита€ правую часть из свободного члена
         int c = c_left - right_side;
-
         coefficients = { a, b, c };
 
         std::cout << "a = " << a << std::endl;
@@ -68,40 +64,3 @@ std::vector<int> getCoefficients(std::string str) {
 
     return coefficients;
 }
-
-
-//std::regex pattern(R"(([+-]?\d*)\*?x\^2([+-]\d*\*?x)?([+-]\d*)?=([+-]?\d+))");
-
-//std::regex pattern(R"(([+-]?\d*)[*]?x^2([+-]?\d*)[*]?x([+-]?\d*)=([+-]?\d+))");
-
-/*
-std::vector<int> getCoefficients(std::string str) {
-    std::vector<int> coefficients;
-    std::regex pattern(R"(\s*([+-]?\d*)x\^2\s*([+-]?\s*\d*)x\s*([+-]?\s*\d*)\s*=\s*([+-]?\d+)\s*)");
-    std::smatch match;
-
-    if (std::regex_match(str, match, pattern)) {
-        int a = match[1].str().empty() || match[1].str() == "+" ? 1 :
-            (match[1].str() == "-" ? -1 : std::stoi(match[1].str()));
-
-        int b = match[2].str().empty() || match[2].str() == "+" ? 1 :
-            (match[2].str() == "-" ? -1 : std::stoi(match[2].str()));
-
-        int c = std::stoi(match[3].str());
-        int right_side = std::stoi(match[4].str())
-
-        c -= right_side;
-
-        coefficients = { a, b, c };
-
-        std::cout << "a = " << a << std::endl;
-        std::cout << "b = " << b << std::endl;
-        std::cout << "c = " << c << std::endl;
-    }
-    else {
-        std::cout << "Incorrect input format!" << std::endl;
-    }
-
-    return coefficients;
-}
-*/
--- main.cpp
+++ main.cpp
@@ -4,5 +4,6 @@ int main() {
     std::string str;
     std::getline(std::cin, str);
     std::vector<int> coefficients = getCoefficients(str);
+    std::cout << "Проверка сопряжения github с VS2022" << std::endl;
     return 0;
 }
