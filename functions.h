97bff5bc4cbc1ae094a20a9bb9200b0b784c0e33
Дмитрий Жданов <D.Zhdanov66@ya.ru>
2025-02-18 13:50:46 +0300

    Настоящая очистка ветки master

--- functions.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include <iostream>
-#include <algorithm>
-#include <array>
-#include <string>
-#include <vector>
-#include <regex>
-
-std::vector<int> getCoefficients(std::string str);
