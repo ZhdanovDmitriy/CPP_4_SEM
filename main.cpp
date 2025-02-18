ec5582eefee83c9e63c94c694bac2475ad127684
Дмитрий Жданов <D.Zhdanov66@ya.ru>
2025-02-18 03:24:01 +0300

    Проверка работы VS2022 с github

--- main.cpp
+++ main.cpp
@@ -4,6 +4,5 @@ int main() {
     std::string str;
     std::getline(std::cin, str);
     std::vector<int> coefficients = getCoefficients(str);
-    std::cout << "Проверка сопряжения github с VS2022" << std::endl;
     return 0;
 }
